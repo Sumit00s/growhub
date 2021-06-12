93f1203069e458e338765d7dc5bc1afcf5cd4787
Sumit Sonawane <79162434+Sumit00s@users.noreply.github.com>
2021-06-12 23:43:33 +0530

    Add initial grow hub website files.

---
+++ index.js
@@ -0,0 +1,20 @@
+
+
+
+// Smooth Scrolling
+$('#nav a').on('click', function(event) {
+    if (this.hash !== '') {
+      event.preventDefault();
+  
+      const hash = this.hash;
+  
+      $('html, body').animate(
+        {
+          scrollTop: $(hash).offset().top - 170
+        },
+        1000
+      );
+    }
+  });
+
+  
